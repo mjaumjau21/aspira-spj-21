b0460121d46afa15f1957f5af1275681f87879e9
ilovric00 <ilovric@profico.hr>
2020-03-11 21:32:39 +0100

    Task-1 completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  handleDecrement() {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simple React example</h1>
+          <button onClick={this.handleIncrement}>INCREMENT</button>
+          <button onClick={this.handleDecrement}>DECREMENT</button>
+        </header>
+        <p className="App-intro">Counter value from state: {this.state.counter}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
