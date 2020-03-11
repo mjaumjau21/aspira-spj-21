a0953f8d949f6278fe9518cd566a59d9dc378d27
ilovric00 <ilovric@profico.hr>
2020-03-11 21:43:39 +0100

    Task-2 completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from "./Button";
 
 class App extends Component {
   constructor(props) {
@@ -8,28 +8,19 @@ class App extends Component {
     this.state = {
       counter: 0
     };
-    this.handleIncrement = this.handleIncrement.bind(this);
-    this.handleDecrement = this.handleDecrement.bind(this);
   }
 
-  handleIncrement() {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
-  handleDecrement() {
-    this.setState({ counter: this.state.counter - 1 });
+  handleIncrement = value => {
+    this.setState({ counter: this.state.counter + value });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React example</h1>
-          <button onClick={this.handleIncrement}>INCREMENT</button>
-          <button onClick={this.handleDecrement}>DECREMENT</button>
-        </header>
-        <p className="App-intro">Counter value from state: {this.state.counter}</p>
+        <Button onIncrement={this.handleIncrement} value={1} />
+        <Button onIncrement={this.handleIncrement} value={10} />
+        <Button onIncrement={this.handleIncrement} value={100} />
+        <p>Counter value from state: {this.state.counter}</p>
       </div>
     );
   }
