a0b130bc6aabbe469d8d08097eacc9e7a8409b42
ilovric00 <ilovric@profico.hr>
2020-05-06 16:47:55 +0200

    Task 24 completed

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { removeCard, bookmarkCard } from '../../redux/actions';
+import { deleteCard } from "../../services";
 import { NavLink } from 'react-router-dom';
 import styles from './Card.module.css';
 import EditForm from '../../containers/EditForm/EditForm';
@@ -19,6 +20,11 @@ class Card extends Component {
     this.setState({ isEditing: !this.state.isEditing });
   }
 
+  handleDelete = async () => {
+    await deleteCard(this.props.card.id);
+    this.props.actions.removeCard(this.props.index);
+  };
+
   render() {
     const cardClass = `${styles.card} ${this.props.index % 2 ? styles.dark : ''}`;
     return (
@@ -27,7 +33,7 @@ class Card extends Component {
         {this.state.isEditing ? (
           <EditForm card={this.props.card} onToggleEditing={this.toggleEditing} />
         ) : (
-          <CardDetails {...this.props} onToggleEditing={this.toggleEditing} />
+          <CardDetails {...this.props} onToggleEditing={this.toggleEditing} onDelete={this.handleDelete} />
         )}
       </div>
     );
@@ -46,7 +52,7 @@ const CardDetails = (props) => {
       </div>
       <button className={styles.favoriteBttn} onClick={() => props.actions.bookmarkCard(props.card.id)}>&#9734;</button>
       <button className={styles.editBttn} onClick={props.onToggleEditing}>&#9998;</button>
-      <button className={styles.closeBttn} onClick={() => props.actions.removeCard(props.index)}>&times;</button>
+      <button className={styles.closeBttn} onClick={props.onDelete}>&times;</button>
     </>
   )
 }
